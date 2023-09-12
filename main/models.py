5bd8634307718fa879d9a0e6ff470b2d861c9907
RaquelNayyara <quelaazzz@gmail.com>
2023-09-12 14:41:08 +0700

    hampir selesai

---
+++ main/models.py
@@ -0,0 +1,7 @@
+# Create your models here.
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    description = models.TextField()
